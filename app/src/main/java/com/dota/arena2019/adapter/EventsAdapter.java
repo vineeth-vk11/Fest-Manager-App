6be9a6cce2766f82ffd3546ac37fca41f637d7b9
prat-bphc52 <hp.agarwal1@gmail.com>
2019-01-20 17:17:22 +0530

    Refractor package name

---
+++ app/src/main/java/com/dota/arena2019/adapter/EventsAdapter.java
@@ -0,0 +1,91 @@
+package com.dota.arena2019.adapter;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.dota.arena2019.R;
+import com.dota.arena2019.activity.DetailsActivity;
+import com.dota.arena2019.model.EventDetails;
+
+import java.util.ArrayList;
+
+public class EventsAdapter extends RecyclerView.Adapter<EventsAdapter.EventViewHolder> {
+
+    private ArrayList<EventDetails> list = new ArrayList<>();
+    private Context context;
+
+    public EventsAdapter(ArrayList<EventDetails> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public EventsAdapter.EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(context).inflate(R.layout.fragment_events_item, parent, false);
+        return new EventViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventsAdapter.EventViewHolder holder, final int position) {
+
+        holder.eventView.setVisibility(View.VISIBLE);
+        holder.eventName.setText(list.get(position).getName());
+        holder.eventTagLine.setVisibility(View.VISIBLE);
+        holder.eventPrizemoney.setVisibility(View.VISIBLE);
+        holder.eventFee.setVisibility(View.VISIBLE);
+        if (list.get(position).getTagline() == null || list.get(position).getTagline().equals("")) {
+            holder.eventTagLine.setVisibility(View.GONE);
+        } else {
+            holder.eventTagLine.setText(list.get(position).getTagline());
+        }
+        if (list.get(position).getPrice() == null || list.get(position).getPrice().equals("")) {
+            holder.eventFee.setVisibility(View.GONE);
+        } else {
+            holder.eventFee.setText("Fee: " + list.get(position).getPrice());
+        }
+        if (list.get(position).getPrize() == null || list.get(position).getPrize().equals("")) {
+            holder.eventPrizemoney.setVisibility(View.GONE);
+        } else {
+            holder.eventPrizemoney.setText("Prize: " + list.get(position).getPrize());
+        }
+
+        holder.eventView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context, DetailsActivity.class);
+                intent.putExtra("id", list.get(position).getId());
+                v.getContext().startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class EventViewHolder extends RecyclerView.ViewHolder {
+        TextView eventName, eventTagLine, eventFee, eventPrizemoney;
+        CardView eventView;
+
+        public EventViewHolder(View itemView) {
+            super(itemView);
+            eventName = itemView.findViewById(R.id.events_name);
+            eventTagLine = itemView.findViewById(R.id.events_tagline);
+            eventFee = itemView.findViewById(R.id.events_fee);
+            eventPrizemoney = itemView.findViewById(R.id.events_prizemoney);
+            eventView = itemView.findViewById(R.id.event_item_cardview);
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/dota/arena2019/fragment/MoreFragment.java
@@ -0,0 +1,50 @@
+package com.dota.arena2019.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.dota.arena2019.R;
+import com.dota.arena2019.adapter.MoreAdapter;
+
+import java.util.ArrayList;
+
+public class MoreFragment extends Fragment {
+    private RecyclerView more_recycler;
+    public ArrayList<String> moreitemsList = new ArrayList<>();
+    private Context context;
+    private String TAG = "MoreFragment";
+
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        context = getContext();
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_more, container, false);
+    }
+
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+
+        moreitemsList.add("About");
+        moreitemsList.add("Reach Us");
+        moreitemsList.add("Directions");
+        moreitemsList.add("Contact us");
+        moreitemsList.add("App Credits");
+        moreitemsList.add("Settings");
+
+        more_recycler = getActivity().findViewById(R.id.more_recycler_view);
+
+        more_recycler.setLayoutManager(new LinearLayoutManager(getActivity()));
+        MoreAdapter adapter = new MoreAdapter(context, moreitemsList);
+        more_recycler.setAdapter(adapter);
+    }
+}
